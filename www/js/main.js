38d3e54e08c8d2f61b008a4d28cde138787ccbe9
jose-carlos-pena <jpena3@saic.edu>
2018-12-11 23:55:22 -0600

    finished the visual comp

--- www/js/main.js
+++ www/js/main.js
@@ -4,7 +4,7 @@ let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHe
 
 let renderer = new THREE.WebGLRenderer()
 renderer.gammaOutput = true
-renderer.gammaFactor = 2.2
+renderer.gammaFactor = 3
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
 
@@ -13,20 +13,14 @@ document.body.appendChild( renderer.domElement )
 let light = new THREE.AmbientLight( 0x404040 ) // soft white light
 scene.add( light )
 
-//load server 3dmodel
-let loader1 = new THREE.GLTFLoader()
+var skyLight = new THREE.HemisphereLight( 0xFFF5F9, 0x080820, 0.5)
+scene.add( skyLight )
+dirLight = new THREE.DirectionalLight( 0xffffff, 1 )
+				dirLight.color.setHSL( 0.1, 1, 0.95 )
+				dirLight.position.set( - 1, 1.75, 1 )
+				dirLight.position.multiplyScalar( 0.5 )
+scene.add( dirLight )
 
-let server
-
-loader1.load('/3dmodel/server.gltf', function ( gltf1 ){
-    server = gltf1.scene
-    scene.add( gltf1.scene )
-    server.scale.set(0.2,0.2,0.2)
-    server.position.y = 0.8
-
-}, undefined, function ( error ) {
-  console.error( error )
-} )
 
 //load 3d model
 let loader = new THREE.GLTFLoader()
@@ -36,35 +30,106 @@ let map
 loader.load( '/3dmodel/goodmap.gltf', function ( gltf ) {
     map = gltf.scene
     scene.add( gltf.scene )
+    map.scale.set( 25, 25, 25 )
+    map.position.y = -15
 }, undefined, function ( error ) {
 	 console.error( error )
 } )
 
-//creating and adding a cube into the scene
-let geometry = new THREE.BoxGeometry( 1, 1, 1 )
-let material = new THREE.MeshNormalMaterial( {light: true} )
-let cube = new THREE.Mesh( geometry, material )
-scene.add( cube )
 
 let texture = new THREE.TextureLoader().load( '/3dmodel/ncloudtx.png' )
 
 //skybox
-let sgeometry = new THREE.BoxGeometry( 15, 15, 15)
+let sgeometry = new THREE.BoxGeometry( 250, 250, 250 )
 let smaterial = new THREE.MeshBasicMaterial( {map: texture, side: THREE.DoubleSide} )
 let skybox = new THREE.Mesh( sgeometry, smaterial )
+skybox.shadow
 scene.add( skybox )
 
+//controls
+controls = new THREE.MapControls( camera, renderer.domElement )
 
-//moving the camera
-camera.position.z = 5
-camera.position.y = 1
+//controls.addEventListener( 'change', render ) // call this only in static scenes (i.e., if there is no animation loop)
+controls.enableDamping = true // an animation loop is required when either damping or auto-rotation are enabled
+controls.dampingFactor = 1
+controls.screenSpacePanning = false
+controls.minDistance = 1
+controls.maxDistance = 20
+controls.maxPolarAngle = 10
+
+//server
+let serverFMap = new THREE.TextureLoader().load( '3dmodel/normals/serverfmap.png')
+let serverFTexture = new THREE.TextureLoader().load( '3dmodel/serverfront.png' )
+let serverFMaterial = new THREE.MeshStandardMaterial( {map: serverFTexture, normalMap: serverFMap} )
+
+let serverFGeometry = new THREE.BoxGeometry( 3, 1, 2)
+let serverFront = new THREE.Mesh( serverFGeometry, serverFMaterial )
+
+let serverSMap = new THREE.TextureLoader().load( '3dmodel/normals/serversmap.png')
+let serverSTexture = new THREE.TextureLoader().load( '3dmodel/servershell.png' )
+let serverSMaterial = new THREE.MeshStandardMaterial( {map: serverSTexture, normalMap: serverSMap} )
+
+let serverSGeometry = new THREE.BoxGeometry( 3.1, 1.1, 3)
+let serverShell = new THREE.Mesh( serverSGeometry, serverSMaterial )
+
+scene.add( serverFront, serverShell )
+
+serverFront.position.z = 0.55
+
+//sparkles
+
+let geometry = new THREE.BufferGeometry()
+let vertices = []
+let sparkle1 = new THREE.TextureLoader().load( 'sprites/sparkle.png' )
+
+for ( let i = 0; i < 30; i ++){
+
+      let x = Math.random() * 10 - 5
+      let y = Math.random() * 10 - 5
+      let z = Math.random() * 10 - 5
+
+      vertices.push( x, y, z);
+}
+
+geometry.addAttribute( 'position', new THREE.Float32BufferAttribute(vertices, 3) )
+
+parameters =[
+  [[ 0.90, 0.05, 0.01 ], sparkle1, 10 ]
+]
+
+for ( let i = 0; i < parameters.length; i++){
+    let color = parameters[ i ][ 0 ]
+    let sprite = parameters[ i ][ 1 ]
+    let size = parameters[ i ][ 2 ]
+
+    let materials = new THREE.PointsMaterial( {size: size, map: sprite, blending: THREE.AdditiveBlending, depthTest: false, transparent: true })
+    materials.color.setHSL( color[ 0 ], color [ 1], color [2] )
+
+    let particles = new THREE.Points( geometry, materials )
+    particles.rotation.x = Math.random() * 3
+    particles.rotation.y = Math.random() * 3
+    particles.rotation.z = Math.random() * 3
+
+    scene.add( particles );
+
+}
+
+
+
+//renderer
+
+function render(){
+
+	renderer.render( scene, camera )
+}
+
+
 
 
 //a function to render the scene
 function animate() {
     requestAnimationFrame( animate )
+
 	  renderer.render( scene, camera )
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 }
-animate();
+animate()
